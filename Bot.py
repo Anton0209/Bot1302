1b69685cf273f9d269f102eeb5ab85982a2149d0
Anton0209 <101557615+Anton0209@users.noreply.github.com>
2022-03-16 09:13:02 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,10 +1,19 @@
 import telebot
+from telebot import types
 token = '5208733321:AAGqzpcwMQs3oM31FmE2Ns9HeCq9d3ExA0E'
 bot = telebot.TeleBot(token)
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id,text="Привет, {0.first_name}!". format(message.from_user))
+    
+@bot.message_handler(commands=['button'])
+def button_message(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1= types.KeyboardButton("Кнопка")
+    markup.add(item1)
+    bot.send_message(message.chat.id,'Выберите что вам надо', reply_markup=markup)
 
 if __name__ == '__main__':
      bot.infinity_polling()
