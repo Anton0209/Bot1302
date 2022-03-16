2dce96a500d580bdb3632a16c786f314d221700f
Anton0209 <101557615+Anton0209@users.noreply.github.com>
2022-03-16 08:58:50 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -2,7 +2,7 @@ import telebot
 token = '5208733321:AAGqzpcwMQs3oM31FmE2Ns9HeCq9d3ExA0E'
 bot = telebot.TeleBot(token)
 
-@bot.message_hendler(commands=['start'])
+@bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id,text="Привет, {0.first_name}!". format(message.from_user))
 
