a9ea079676e8c68c4c1443a6114ff9ccef989803
Anton0209 <101557615+Anton0209@users.noreply.github.com>
2022-03-16 08:54:14 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -2,9 +2,10 @@ import telebot
 token = '5208733321:AAGqzpcwMQs3oM31FmE2Ns9HeCq9d3ExA0E'
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, message.text)
+@bot.message_hendler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id,text="Привет, {0.first_name}!". format(message.from_user))
 
 if __name__ == '__main__':
      bot.infinity_polling()
+        
