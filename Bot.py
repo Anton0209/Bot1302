0f93b334a3a275eb54b63009df01cb2baadd6a58
Anton0209 <101557615+Anton0209@users.noreply.github.com>
2022-03-21 13:03:16 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -8,21 +8,34 @@ bot = telebot.TeleBot(token)
 def start_message(message):
     bot.send_message(message.chat.id,text="Привет, {0.first_name}!". format(message.from_user))
     
+    
 @bot.message_handler(commands=['button'])
 def button_message(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1= types.KeyboardButton("vk")
-    item2= types.KeyboardButton("instagram")
+    item1= types.KeyboardButton("Игрок идет налево")
+    item2= types.KeyboardButton("Игрок идет направо")
     markup.add(item1, item2)
-    bot.send_message(message.chat.id,'Выберите что вам надо', reply_markup=markup)
+    bot.send_message(message.chat.id,'Выберите куда идти', reply_markup=markup)
     
+ 
     
 @bot.message_handler(content_types='text')
 def message_reply(message):
-    if message.text=="vk":
-        bot.send_message(message.chat.id,"https://vk.com/")
-    elif message.text=="instagram":
-        bot.send_message(message.chat.id,"https://www.instagram.com/")
+    if message.text=="Игрок идет налево":
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1= types.KeyboardButton("Взять тяжелое оружие")
+        item2= types.KeyboardButton("Взять легкое оружие")
+        item3= types.KeyboardButton("Взять холодное оружие")
+        markup.add(item1, item2, item3)
+        bot.send_message(message.chat.id,"Можете снарядиться", reply_markup=markup)
+     else message.text=="Игрок идет направо":
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1= types.KeyboardButton("автомобиль")
+        item2= types.KeyboardButton("танк") 
+        markup.add(item1, item2)
+        bot.send_message(message.chat.id,"Можете выбрать транспорт", reply_markup=markup)
+        
+
 
 
 if __name__ == '__main__':
